f5b9f4dc2784968a69bcfcaa7007d05f8ce8ccda
arthurchan35 <arthurchan35@gmail.com>
2017-03-30 17:31:18 +0800

    webgl test

--- Kenken/src/main/java/Kenken/portlet/KenkenPortlet.java
+++ Kenken/src/main/java/Kenken/portlet/KenkenPortlet.java
@@ -7,13 +7,15 @@ import javax.portlet.Portlet;
 import org.osgi.service.component.annotations.Component;
 
 /**
- * @author liferay
+ * @author arthurchan35
  */
 @Component(
 	immediate = true,
 	property = {
 		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.instanceable=true",
+		"com.liferay.portlet.header-portlet-css=/css/webgl.css",
+		"com.liferay.portlet.footer-portlet-javascript=/js/webgl.js",
 		"javax.portlet.display-name=Kenken Portlet",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
